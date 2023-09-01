1e913ff89cbe9a6387a11ae6ce96cfe49ad90b61
Savi Shet Shirodkar <95627043+savishirodkar@users.noreply.github.com>
2023-09-01 12:30:23 +0530

    Delete crm_backend directory

--- crm_backend/crm_apps/core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import *
-
-class MemberAdmin(admin.ModelAdmin):
-  list_display = ('username', 'email', 'role', 'is_active', 'is_staff')
-  search_fields = ['username', 'email']
-
-admin.site.register(CustomUser, MemberAdmin)
